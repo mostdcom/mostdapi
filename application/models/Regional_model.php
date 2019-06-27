3b5f724f1f952c83ddf128dfdaa9ebf81eb02cfb
Sofyan Thayf <sofyanthayf@gmail.com>
2019-06-27 14:36:28 +0800

    kodepos distinct

--- application/models/Regional_model.php
+++ application/models/Regional_model.php
@@ -4,6 +4,7 @@ class Regional_model extends CI_Model {
 
   public function getKodePosByDesa($desa)
   {
+    $this->db->distinct();
     $this->db->like('kelurahan', $desa);
     $query = $this->db->get('regional_kodepos');
     return $query->result();

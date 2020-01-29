cf2105fe5b4ea24a09fe7314372b69f0dd131b95
Sofyan Thayf <sofyanthayf@gmail.com>
2020-01-29 11:43:27 +0800

    Layanan Regional Desa

--- application/models/Regional_model.php
+++ application/models/Regional_model.php
@@ -9,7 +9,7 @@ public function getListDesaByName($desa)
   $this->db->like('d.name', $desa);
   $this->db->join('regional_kecamatan c','c.id=d.district_id', 'left');
   $this->db->join('regional_kabkota k','k.id=c.regency_id', 'left');
-  $this->db->join('regional_propinsi p','p.id=k.provence_id', 'left');
+  $this->db->join('regional_propinsi p','p.id=k.province_id', 'left');
   $query = $this->db->get('regional_desa d');
   return $query->result();
 }
